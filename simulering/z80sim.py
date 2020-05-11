f4b7f8b8ac3b4722544658437134c96c2cc6fb87
JAndersM <jandersmartinsson@gmail.com>
2020-05-11 11:54:42 +0200

    Sim settings easier to spot/change

--- simulering/z80sim.py
+++ simulering/z80sim.py
@@ -4,6 +4,11 @@ from periferials import seg7x4
 from pathlib import Path
 import os
 
+#Settings
+logging = False
+maxclock= -1 #-1 no limit
+verbose=False
+
 #Simulation main loop
 #Logging on or of
 def main(log=True):
@@ -27,7 +32,7 @@ def main(log=True):
         z80=Z80CPU(m, io, cpulog)
         m.initROM(0, code)
         
-        z80.run(300, False)
+        z80.run(maxclock, verbose)
         if z80.mHALT :
             print("Halted after "+ str(z80.clk) + " cykles")
 
@@ -35,7 +40,7 @@ def main(log=True):
 
 
 if __name__ == "__main__":
-    main(True)         
+    main(logging)         
         
         
         
